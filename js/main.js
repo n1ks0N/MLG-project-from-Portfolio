b0a36ad6baf6d051b292901303cbfac306150cb4
niksON <gromyakov05@mail.ru>
2020-04-27 19:27:51 +0300

    master

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
+    
+    // in blog
+    
     $('.main__text').click(function() {
         window.location.href = 'about.html';
     });
     $('.main__img').click(function() {
         window.location.href = 'about.html';
     });
+
+    // services
+
+    $('#business').click(function() {
+        $('.services__business').removeClass('services-content_passive');
+        $('.services__individual').addClass('services-content_passive');
+    });
+    $('#individual').click(function() {
+        $('.services__business').addClass('services-content_passive');
+        $('.services__individual').removeClass('services-content_passive');
+    })
 });
