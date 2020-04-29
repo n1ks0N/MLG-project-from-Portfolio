785ae1a5f696dd16a2f071815e8f54750b35e9d7
niksON <gromyakov05@mail.ru>
2020-04-29 13:00:17 +0300

    master

--- js/main.js
+++ js/main.js
@@ -15,18 +15,18 @@ $(document).ready(function() {
         $('.services__business').removeClass('services-content_passive');
         $('.services__individual').addClass('services-content_passive');
 
-        $(this).addClass('btn-info');
-        $(this).removeClass('btn-outline-info');
-        $('#individual').addClass('btn-outline-info');
-        $('#individual').removeClass('btn-info');
+        $(this).addClass('btn-primary');
+        $(this).removeClass('btn-outline-primary');
+        $('#individual').addClass('btn-outline-primary');
+        $('#individual').removeClass('btn-primary');
     });
     $('#individual').click(function() {
         $('.services__business').addClass('services-content_passive');
         $('.services__individual').removeClass('services-content_passive');
 
-        $(this).removeClass('btn-outline-info');
-        $(this).addClass('btn-info');
-        $('#business').addClass('btn-outline-info');
-        $('#business').removeClass('btn-info');
+        $(this).addClass('btn-primary');
+        $(this).removeClass('btn-outline-primary');
+        $('#business').addClass('btn-outline-primary');
+        $('#business').removeClass('btn-primary');
     })
 });
